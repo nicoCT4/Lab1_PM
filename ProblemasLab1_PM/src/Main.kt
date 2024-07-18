951ddd4635f68ce7d34fdcb8d2e3f4ddc256b88b
Nicolás <con23197@uvg.edu.gt>
2024-07-17 22:03:55 -0600

    Creacion del problema 1

---
+++ ProblemasLab1_PM/src/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    fun problema1(){
+        println("Ingrese el valor decimal")
+        val numero = readLine()
+        var valorNumero = numero?.toIntOrNull()
+        var binario = 0
+
+        while (valorNumero > 0 {
+            val resultado = valorNumero % 2
+                binario += resultado
+            }
+
+
+    }
+    problema1()
+}
